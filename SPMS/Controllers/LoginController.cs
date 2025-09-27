fb8b41a63b34e4e670981e0572e298bf56bdf0d3
Siddharth <sid_patel14@yahoo.com>
2025-09-27 13:36:20 +0100

    Citizen Dashboard,Login and Register view pages

---
+++ SPMS/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using SPMS.Models;
+
+
+
+
+namespace SPMS.Controllers
+{
+
+    public class LoginController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public LoginController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult CitizenLogin()
+        {
+            return View();
+        }
+
+        
+
+ 
+    }
+}
+
+
+
